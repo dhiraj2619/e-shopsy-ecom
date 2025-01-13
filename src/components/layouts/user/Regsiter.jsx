d18546843c701302b0c06821437c3bafae91c4fb
dhiraj <dhirajmymodak22@gmail.com>
2025-01-13 18:21:33 +0530

    image error fixed

--- src/components/layouts/user/Regsiter.jsx
+++ src/components/layouts/user/Regsiter.jsx
@@ -114,7 +114,7 @@ const Regsiter = () => {
                                 <div className="flex flex-col w-full justify-between sm:flex-row gap-3 items-center mt-3">
                                     <Avatar src={avatarPreview} alt='Avatar Preview' sx={{ height: 56, width: 56 }} />
                                     <label  className="rounded font-medium bg-gray-400 text-center cursor-pointer text-white w-full py-2 px-2.5 shadow hover:shadow-lg">
-                                        <input type="file" name="avatar" accept="image/*" className="hidden" />Choose File
+                                        <input type="file" name="avatar" accept="image/*" className="hidden" onChange={handleDataChange} />Choose File
                                     </label>
                                 </div>
                                 <button type="submit" className="text-white py-3 w-full bg-black rounded-md">Signup</button>
